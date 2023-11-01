af19d9c1ce73387811581c760f1ed470c16e438e
Guilherme Henrique <gh.dourado77@gmail.com>
2023-11-01 16:16:28 -0300

    feat: skeleton component for the loading

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await api("/products/featured", {
+    next: {
+      revalidate: 60 * 60,
+    },
+  });
 
   const products = await response.json();
 
