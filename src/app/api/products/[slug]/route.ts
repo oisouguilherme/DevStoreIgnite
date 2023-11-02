3611ffbb8430afde5b66499daa4404593ac12d25
Guilherme Henrique <gh.dourado77@gmail.com>
2023-11-02 12:00:27 -0300

    feat: add rota api de produto

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import data from "../data.json";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  const slug = z.string().parse(params.slug);
+
+  const product = data.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    return Response.json(
+      { message: "Produto não encontrado" },
+      { status: 400 }
+    );
+  }
+
+  return Response.json(product);
+}
