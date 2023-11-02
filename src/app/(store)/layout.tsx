214910cd695890680577bb8df2d766bf65a726c1
Guilherme Henrique <gh.dourado77@gmail.com>
2023-11-02 12:34:52 -0300

    feat: context addToCart

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@/components/header";
+import { CartProvider } from "@/context/cart-context";
 
 export default function StoreLayout({ children }: any) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-6xl grid-rows-[min-content_max-content] gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-6xl grid-rows-[min-content_max-content] gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
